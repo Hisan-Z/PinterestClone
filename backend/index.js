962b2e1f821ac52b318c80c9b9cb2e3f64a0543d
Hisan-Z <hisansunasara007@gmail.com>
2023-11-20 21:28:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ app.use(cors({
     origin:["https://pinterest-clone-sooty-theta.vercel.app"],
     methods:["GET","POST","DELETE","PUT"]
 }))
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://pinterest-clone-sooty-theta.vercel.app");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 app.use(bodyParser.json());
 app.use('/api/user', userRoutes)
 app.use('/api/pins', pinRoutes)

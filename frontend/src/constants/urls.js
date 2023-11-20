eb70002f26cc787b4e297b52487bffc79623b143
Hisan-Z <hisansunasara007@gmail.com>
2023-11-20 19:14:26 +0530

    Update urls.js

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,9 +1,8 @@
-export const SERVER_URL = 'http://localhost:5000/api'
 
-export const SERVER_USER_URL = SERVER_URL + '/user'
-export const SERVER_PIN_URL = SERVER_URL + '/pins'
-export const SERVER_COMMENT_URL = SERVER_URL + '/comment'
-export const SERVER_LIKES_URL = SERVER_URL + '/like'
+export const SERVER_USER_URL = process.env.SERVER_URL + '/user'
+export const SERVER_PIN_URL = process.env.SERVER_URL + '/pins'
+export const SERVER_COMMENT_URL = process.env.SERVER_URL + '/comment'
+export const SERVER_LIKES_URL = process.env.SERVER_URL + '/like'
 
 
 export const DEFAULT_IMG_URL="https://png.pngtree.com/png-vector/20190820/ourmid/pngtree-no-image-vector-illustration-isolated-png-image_1694547.jpg"

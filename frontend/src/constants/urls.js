5ba6fd5637fcec0e722e43a91f27a383c635f48d
Hisan-Z <hisansunasara007@gmail.com>
2023-11-20 20:26:51 +0530

    Update urls.js

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,11 +1,8 @@
-import dotenv from "dotenv"
 
-dotenv.config()
-
-export const SERVER_USER_URL = process.env.BASE_URL + '/user'
-export const SERVER_PIN_URL = process.env.BASE_URL + '/pins'
-export const SERVER_COMMENT_URL = process.env.BASE_URL + '/comment'
-export const SERVER_LIKES_URL = process.env.BASE_URL + '/like'
+export const SERVER_USER_URL = process.env.REACT_APP_BASE_URL + '/user'
+export const SERVER_PIN_URL = process.env.REACT_APP_BASE_URL + '/pins'
+export const SERVER_COMMENT_URL = process.env.REACT_APP_BASE_URL + '/comment'
+export const SERVER_LIKES_URL = process.env.REACT_APP_BASE_URL + '/like'
 
 
 export const DEFAULT_IMG_URL="https://png.pngtree.com/png-vector/20190820/ourmid/pngtree-no-image-vector-illustration-isolated-png-image_1694547.jpg"
